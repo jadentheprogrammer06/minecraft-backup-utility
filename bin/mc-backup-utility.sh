21497095078dc1c37c2dea5bc4bb4f78a3a8cb9c
jadentheprogrammer <jadentheprogrammer06@github.com>
2020-08-28 13:28:15 -0400

    Updating scripts.

---
+++ bin/mc-backup-utility.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# NOTE:
+# This script will not run if any process containing the string "minecraft" is found. This may include the script itself or minecraft-launcher for example.
+# For this reason, the executable script should be renamed to "mc-backup-utility". Also make sure to close any minecraft process that is running.
+
+# variables.
+PC_USERNAME=`whoami`
+WORKING_DIR=`pwd`
+MINECRAFT_RUNNING=`pgrep minecraft`
+# directory variables. Modify these to suit your needs.
+DOTMINECRAFT="/home/${PC_USERNAME}/.minecraft"
+BACKUP_FOLDER="/home/${PC_USERNAME}/Documents/backup-dotminecraft"
+
+# exit if supplied .minecraft directory isn't found or if any minecraft process is found currently running.
+[[ -d ${DOTMINECRAFT} ]] && echo "${DOTMINECRAFT} directory existent." || DIR_NOT_FOUND=1
+[[ ${DIR_NOT_FOUND} == 1 ]] && echo "${DOTMINECRAFT} directory non-existent. You can change the .minecraft location used, if you use a custom directory, OR, you do not have minecraft installed." && exit
+[[ ${MINECRAFT_RUNNING} ]] && echo "minecraft process running; not backing up saves while game is running in order to avoid corruption of data." && exit || BACKUP_FILES=1
+
+if [[ ${BACKUP_FILES} == 1 ]] 
+then
+    echo "backing up ${DOTMINECRAFT} directory into ${BACKUP_FOLDER}"
+    [[ -d ${BACKUP_FOLDER} ]] && echo "${BACKUP_FOLDER} exists. this is your backup folder." || BACKUP_NOT_FOUND=1
+    [[ ${BACKUP_NOT_FOUND} == 1 ]] && echo "${BACKUP_FOLDER} non-existent; creating ${BACKUP_FOLDER} folder." && mkdir -p ${BACKUP_FOLDER}
+    echo "copying .minecraft folder and its files..."
+    [[ ${BACKUP_NOT_FOUND} == 0 ]] && rm ${BACKUP_FOLDER}/* -r && echo "Removed previous backup."
+    cp ${DOTMINECRAFT} ${BACKUP_FOLDER}/.minecraft -r && echo '.minecraft folder copied successfully. Note: enable "show hidden files" in your file-browser to view your .minecraft folder.'
+fi
--- install-mcbackup.sh
+++ install-mcbackup.sh
@@ -1,4 +1,5 @@
 INSTALL_PATH='/usr/local/bin'
+SCRIPT='mc-backup-utility'
 printf "\nInstalling minecraft-backup-utility to ${INSTALL_PATH}.\n"
-sudo chmod +x ./bin/minecraft-backup-utility.sh
-sudo cp -p bin/minecraft-backup-utility.sh ${INSTALL_PATH}/mc-backup-utility && printf "\nInstalled successful.\nUse this command: mc-backup-utility.\n"
+sudo chmod +x ./bin/${SCRIPT}.sh
+sudo cp -p bin/${SCRIPT}.sh ${INSTALL_PATH}/${SCRIPT} && printf "\nInstalled successful.\nUse this command: mc-backup-utility.\n"
